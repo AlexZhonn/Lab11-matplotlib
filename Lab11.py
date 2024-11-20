e5ce073dab9051c61b0ef2f5709f7f601d9f8d32
AlexZhonn <zhuen.zhong@outlook.com>
2024-11-20 00:43:04 -0500

    Lab 11

--- Lab11.py
+++ Lab11.py
@@ -9,50 +9,51 @@ def program():
     option = input('Enter your selection: ')
 
     if option == '1':
+        # check if the input students is in the students and store the ID.
         name = input('What is the student\'s name: ')
-        # check if the input students is in the students
+        
         updated_name = name.split()
+        ID = None
         with open('data/students.txt') as students_file:
             students_file_list = students_file.readlines()
             for i in range(len(students_file_list)):
                 SPL = students_file_list[i].split()
                 if updated_name[1] == SPL[1]:
-                    if SPL[0][len(SPL[0])-len(updated_name[0]) : len(SPL[0])+1] == updated_name[0]:
-                        ID = SPL[0][0:len(SPL[0])-len(updated_name[0])]
-                    else:
-                        print("Student not found")
-                        return
-                        
-#find the assignment ID from the submission.txt                     
-        AID = []
+                    if SPL[0][3:] == updated_name[0]:
+                        ID = SPL[0][:3]
+        if not ID:
+            print("Student not found")
+            return
+        
+#find the assignment ID from the submission.txt and weight  
+        AID_weight_dic = {}             
+        AID = None
         students_weight = []
         for i in os.listdir('data/submissions'):    
             with open(f"data/submissions/{i}") as file:
                 content = file.readline()
                 if content.split()[0][:3] == ID:
-                    AID.append(content.split()[0][4:9])
-                    students_weight.append(content.split()[0][-2:])
-        print(students_weight)
-                    
-# find the weight from assignments.txt
-        points = []
-        with open('data/assignments.txt') as afile:
-            afile.list = afile.readlines()
-            for i in range(len(afile.list)):
-                for j in AID:
-                    if afile.list[i].strip() == j:
-                        points.append(afile.list[i+1].strip())
-                    elif j[:-1] == afile.list[i]:
-                        points.append(afile.list[i+1].strip())
+                    AID = content.split()[0][4:9]
+                    if AID[-1] == '|':
+                        AID = AID[:-1]
+                    students_weight = content.split()[0][-2:]
+                    AID_weight_dic[AID] = int(students_weight)
 
-        print(points)
+# find the point from assignments.txt
+        AID_points_dic = {}
+        points = None
+        with open('data/assignments.txt') as afile:
+            afile_list = afile.readlines()
+            for i in range(len(afile_list)):
+                for j in AID_weight_dic.keys():
+                    if afile_list[i].strip() == j:
+                        points = afile_list[i+1].strip()
+                        AID_points_dic[j] = int(points)
 #Calculate Student's grade by multiplying the weight and percentage
         total = 0
-        total_points = [(float(point)) * (float(weight)/100) for point , weight in zip(points , students_weight)]
-        print(total_points)
-        for total_point in total_points:
-            total += total_point
-        print(total/1000)
+        total_points = {key: AID_weight_dic[key] * AID_points_dic[key] for key in AID_weight_dic if key in AID_points_dic}
+        total = sum(total_points.values())
+        print(f'{round(total/1000)}%')
 
 
     elif option == '2':
@@ -75,7 +76,6 @@ def program():
                     grade.append(int(content.split()[0][-2:]))
                 elif content.split()[0][4:8] == Ass_ID:
                     grade.append(int(content.split()[0][-2:]))
-        print(grade)
         min_value= min(grade)
         max_value = max(grade)
         for i in grade:
